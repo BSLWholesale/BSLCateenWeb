90f5ec2d8cffa52b2b182ae16980ba94b62cfce8
BSLWholesale <125240695+BSLWholesale@users.noreply.github.com>
2024-08-05 14:31:28 +0530

    BSL HR First Commit

---
+++ BSLHR/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using BSLHR.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BSLHR.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult Fn_LogIn_Employee(clsEmployee objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLHRApiURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Employee/Fn_LogIn_Employee", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No data found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+    }
+}
---
+++ BSLHR/Models/clsEmployee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BSLHR.Models
+{
+    public class clsEmployee
+    {
+        public Int64 EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpLastName { get; set; }
+        public string EmpEmailId { get; set; }
+        public string EmpMobile { get; set; }
+        public Nullable<DateTime> DOB { get; set; }
+        public int EmpGrade { get; set; }
+        public string EmpLocation { get; set; }
+        public int BSLTravelDesk { get; set; }
+        public string EmpPassword { get; set; }
+    }
+    public class ExpenseClaimMaster
+    {
+        public Int64 ExpenseID { get; set; }
+        public Int64 EmpId { get; set; }
+        public DateTime AppliedDate { get; set; }
+        public string ApproveStatus { get; set; }
+    }
+    public class ExpenseClaimDetail
+    {
+        public int EDetId { get; set; }
+        public Int64 ExpenseID { get; set; }
+        public int EModeId { get; set; }
+        public Int64 TravelId { get; set; }
+        public float EAmount { get; set; }
+        
+        public string Remarks { get; set; }
+    }
+
+    public class clsExpenseMode
+    {
+        public int EModeId { get; set; }
+        public string EModeDescription { get; set; }
+    }
+
+    public class clsTravelDeskMaster
+    {
+        public int TDeskId { get; set; }
+        public string TDeskDescription { get; set; }
+        public string TDeskLocation { get; set; }
+    }
+    public class clsTravelMode
+    {
+        public int TModeId { get; set; }
+        public string TravelDescription { get; set; }
+    }
+
+    public class clsTravelRequestDetail
+    {
+        public int TRDetId { get; set; }
+        public Int64 TravelID { get; set; }
+        public int TravelType { get; set; }
+
+        public int TModeId { get; set; }
+        public float Amount { get; set; }
+        public int Qty { get; set; }
+    }
+}
