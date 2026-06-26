45ce16ba312ef2ffc64fa31e8c38e18b4941f31b
BSLWholesale <125240695+BSLWholesale@users.noreply.github.com>
2026-06-26 11:08:08 +0530

    Delete BSLHRAPI directory

--- BSLHRAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BSLHRAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- BSLHRAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BSLHRAPI.WebApiApplication" Language="C#" %>
